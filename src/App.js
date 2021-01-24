c5dbe804119db8f6435902660d5c366b8e362aa7
jina <wlsdk2769@ajou.ac.kr>
2021-01-24 19:18:28 +0900

    Fetch data from movie API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -6,10 +7,11 @@ class App extends React.Component {
     isLoading: true,
     movies: [],
   };
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+  getMovies = async () => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+  };
+  async componentDidMount() {
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
